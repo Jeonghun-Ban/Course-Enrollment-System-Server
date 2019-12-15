0d0c11cde0bed7a48cc9907769f90008c2b4bd6e
Jeonghun-Ban <bjh5230@gmail.com>
2019-12-16 01:21:33 +0900

    maintenance

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,29 +9,11 @@ import Cource.CDirectory;
 import Cource.CLecture;
 import Enrollment.CApply;
 import Enrollment.CBasket;
-import Framework.ICApply;
-import Framework.ICBasket;
-import Framework.ICDirectory;
-import Framework.ICLecture;
-import Framework.ICLogin;
 
 public class Main {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
-        CLogin clogin = new CLogin();
-        CDirectory cDirectory = new CDirectory();
-        CLecture cLecture = new CLecture();
-        CApply cApply = new CApply();
-        CBasket cBasket = new CBasket();
-
-        ICLogin iCLogin;
-        ICDirectory iCDirectory;
-        ICLecture iCLecture;
-        ICApply iCApply;
-        ICBasket iCBasket;
-
         try {
             ServerSocket serverSocket = new ServerSocket(10000);
             while (true) {
